778b0e027f3cc2c62206d1687bbf1f5e5b00a274
Brian Pompey <brianthescholar@gmail.com>
2025-02-19 17:50:35 -0500

    updated link

--- pages/confirm.js
+++ pages/confirm.js
@@ -3,8 +3,9 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "YOUR_SUPABASE_URL"; // Replace
-const supabaseAnonKey = "YOUR_SUPABASE_ANON_KEY"; // Replace
+const supabaseUrl = "https://oddulakvdkuqjhlnzwil.supabase.co"; // Replace
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kZHVsYWt2ZGt1cWpobG56d2lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5OTg4MTgsImV4cCI6MjA1NTU3NDgxOH0.SXAAdh6ZOqnofDOlUEjo_o5cBskuxI9DdmJpxczdyaY"; // Replace
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const Confirm = () => {
